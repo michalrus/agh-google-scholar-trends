2d33e3afe3068a824f9bd12f7a03ed338fe2dd83
Michał Rus <m@michalrus.com>
2013-12-07 04:26:37 +0000

    Fix CookieHttp -- some websites return nullary domain o_O

--- src/main/scala/edu/agh/gst/crawler/CookieHttp.scala
+++ src/main/scala/edu/agh/gst/crawler/CookieHttp.scala
@@ -20,22 +20,27 @@ package edu.agh.gst.crawler
 object CookieHttp {
   import collection.mutable
   import dispatch._, Defaults._
-  import com.ning.http.client.Cookie
   import Crawler._
 
-  private val cookies = new mutable.HashMap[(String, String), Cookie]
+  private case class CookieKey(domain: String, name: String)
+  private case class CookieVal(value: String)
+
+  private val cookies = new mutable.HashMap[CookieKey, CookieVal] with
+    mutable.SynchronizedMap[CookieKey, CookieVal]
+
+  private def hostOfUrl(u: String): String = { // host("htttttps://www.www.google.com/asd") == "www.www.google.com"
+    val np = (u splitAt ((u indexOf "//") + 2))._2
+    val i = np indexOf "/"
+    if (i < 0) np
+    else (np splitAt i)._1
+  }
 
   private def addCookies(req: Req): Req = {
-    def host(u: String) = { // host("htttttps://www.www.google.com/asd") == "www.www.google.com"
-    val np = (u splitAt ((u indexOf "//") + 2))._2
-      val i = np indexOf "/"
-      if (i < 0) np
-      else (np splitAt i)._1
+    val raw = cookies filter {
+      case (k, _) => hostOfUrl(req.url) endsWith k.domain
+    } map {
+      case (k, v) => k.name + "=" + v.value
     }
-
-    def fromDomain(c: Cookie) = host(req.url) endsWith c.getDomain
-
-    val raw = cookies.values filter fromDomain map (c => c.getName + "=" + c.getValue)
 
     if (raw.isEmpty) req
     else req <:< Map("Cookie" -> (raw mkString "; "))
@@ -51,7 +56,8 @@ object CookieHttp {
     Http(svc) flatMap { resp =>
       import collection.JavaConversions._
       resp.getCookies foreach { c =>
-        cookies += (c.getDomain, c.getName) -> c
+        val domain = Option(c.getDomain) getOrElse hostOfUrl(svc.url)
+        cookies += CookieKey(domain, c.getName) -> CookieVal(c.getValue)
       }
       Future(resp)
     }
